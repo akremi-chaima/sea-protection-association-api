084244d6e6ef567b74c2c9079519aac8a7721edd
Chaima <chaima.akremi.1997@gmail.com>
2024-02-29 21:24:34 +0100

    [EVOL] - Manage participant

--- resources/src/Manager/ParticipantManager.php
+++ resources/src/Manager/ParticipantManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Manager;
 
+use App\Entity\Event;
 use App\Entity\Participant;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -24,5 +25,24 @@ class ParticipantManager extends AbstractManager
     public function save(Participant $participant) {
         $this->getEntityManager()->persist($participant);
         $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @param Event $event
+     * @return int
+     */
+    public function count(Event $event) {
+        try {
+            $queryBuilder =  $this->getEntityManager()->createQueryBuilder()
+                ->select('count(participant.id)')
+                ->from(Participant::class, 'participant')
+                ->join(Event::class, 'event', 'WITH', 'event = participant.event')
+                ->andWhere('event = :event')
+                ->setParameter(':event', $event);
+
+            return intval($queryBuilder->getQuery()->getSingleScalarResult());
+        } catch (\Exception $exception) {
+            return 0;
+        }
     }
 }
--- resources/src/Serializer/Event/EventNormalizer.php
+++ resources/src/Serializer/Event/EventNormalizer.php
@@ -2,10 +2,21 @@
 namespace App\Serializer\Event;
 
 use App\Entity\Event;
+use App\Manager\ParticipantManager;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class EventNormalizer implements NormalizerInterface
 {
+    private ParticipantManager $participantManager;
+
+    /**
+     * @param ParticipantManager $participantManager
+     */
+    public function __construct(ParticipantManager $participantManager)
+    {
+        $this->participantManager = $participantManager;
+    }
+
     /**
      * @param Event $event
      * @param string|null $format
@@ -19,6 +30,7 @@ class EventNormalizer implements NormalizerInterface
             'address' => $event->getAddress(),
             'title' => $event->getTitle(),
             'date' => $event->getDate()->format('d/m/Y H:i'),
+            'participants' => $this->participantManager->count($event)
         ];
     }
 
